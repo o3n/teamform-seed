e5093cbe051c25fb94e4e7a04c3a82c1a891b36b
蔡丰宇 <caifengyu@caifengyudeMacBook-Pro.local>
2016-11-27 13:51:00 +0800

    notification change

--- app/TXR/index.js
+++ app/TXR/index.js
@@ -5,7 +5,7 @@ var app = angular.module("mainApp", ["ngRoute", "firebase", "ngCookies"]);
 app.config(function($routeProvider){
      $routeProvider.when("/",{templateUrl:"MyProfile.html", controller:"profileController"})
 
-     .when("/MyNotifications",{templateUrl:"MyNotifications.html"})
+     .when("/MyNotifications",{templateUrl:"MyNotifications.html", controller:'NotificationController'})
 
      .when("/MyEvents/:p",{templateUrl:"MyEvents.html", controller:"EventController"})
      .when("/MyConversation/:p",{templateUrl:"MyConversations.html", controller:"ConversationController"});
@@ -76,13 +76,48 @@ app.controller("EventController",function($scope,$routeParams,$firebaseObject, $
       //  })
       //       return photo;
       // }
-
-      
-
-
-
-
 });
+app.controller("NotificationController", function($scope, $firebaseObject, $firebaseArray){
+      $scope.user = "kid2";
+      var userlist = $firebaseObject(firebase.database().ref("userList"));
+      userlist.$loaded(function(){
+        $scope.notification = userlist[$scope.user]["notification"];
+      })
+
+
+      // $scope.userList=[
+   //           { Name: "Peter", type: "Invitation", Info: ""},
+   //           { Name: "Jason", type: "Application", Info: ""},
+   //           { Name: "Kevin", type: "Information", Info: ""}
+   //     ];
+      // var i;
+      // for(i=0; i<$scope.userList.length; ++i){
+      //   if($scope.userList[i].type=="Invitation"){
+      //     $scope.userList[i].Info="He invites you into the team.";
+      //   }
+      //   else if($scope.userList[i].type=="Application"){
+      //     $scope.userList[i].Info="He wants to join your team.";
+      //   }
+      //   else $scope.userList[i].Info="Someone left the team.";
+
+      // }
+
+      // $scope.whetherfromteam=function(user){
+      //       if($scope.userList[user].type=="Information"){
+      //         return false;
+      //       }
+      //       return true;
+      // }
+
+      // $scope.removeuser=function(user){
+      //       $scope.userList.splice(user,1);
+      // }
+
+
+
+   /*   $scope.adduser=function(){
+      }*/
+  });
 app.controller("ConversationController",function($scope,$routeParams){
         //   $scope.param = $routeParams.p;
             $scope.IsVisiblePeter = false;
@@ -99,40 +134,7 @@ app.controller("ConversationController",function($scope,$routeParams){
 });
 
 
-app.controller("NotificationController", function($scope){
-			$scope.userList=[
-      				{ Name: "Peter", type: "Invitation", Info: ""},
-      				{ Name: "Jason", type: "Application", Info: ""},
-      				{ Name: "Kevin", type: "Information", Info: ""}
-       ];
-      var i;
-      for(i=0; i<$scope.userList.length; ++i){
-      	if($scope.userList[i].type=="Invitation"){
-      		$scope.userList[i].Info="He invites you into the team.";
-      	}
-      	else if($scope.userList[i].type=="Application"){
-      		$scope.userList[i].Info="He wants to join your team.";
-      	}
-      	else $scope.userList[i].Info="Someone left the team.";
 
-      }
-
-      $scope.whetherfromteam=function(user){
-            if($scope.userList[user].type=="Information"){
-              return false;
-            }
-            return true;
-      }
-
-      $scope.removeuser=function(user){
-       			$scope.userList.splice(user,1);
-      }
-
-
-
-   /*   $scope.adduser=function(){
-      }*/
-  });
 
 app.controller("clickCtrl",
     function($scope, $firebaseObject, $firebaseArray, $cookies) {
